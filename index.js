b30c2cdc699161bb169980c6268eed294d16c831
Andrew Rivera <drewisatlas@gmail.com>
2018-05-04 02:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
 
 function deleteFromObjectByKey(recipes, key) {
   var newRecipe = Object.assign({}, recipes)
-  delete newObject[key]
+  delete newObj [key]
   
 } // failing
 
