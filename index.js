308a7cef9a4de03f9ea66374081708748a809fdd
Andrew Rivera <drewisatlas@gmail.com>
2018-05-04 02:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(recipes, key, value)
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  return Object.assign({}, recipes, {[key] : value})
+}
+
